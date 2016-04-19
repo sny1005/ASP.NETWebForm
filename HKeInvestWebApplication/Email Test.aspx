a0be26f61be731190962c9443f3764d5654b5c51
csunny95 <sunny@csunny.net>
2016-04-19 16:07:59 +0800

    Email Server Email Test.aspx

---
+++ HKeInvestWebApplication/Email Test.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Email Test.aspx.cs" Inherits="HKeInvestWebApplication.Email_Test" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+</asp:Content>
---
+++ HKeInvestWebApplication/Email Test.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+
+namespace HKeInvestWebApplication
+{
+    public partial class Email_Test : System.Web.UI.Page
+    {
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient emailServer = new SmtpClient("smtp.cse.ust.hk");
+            mail.From = new MailAddress("lychowaa@cse.ust.hk", "<Sunny Chow>");
+            mail.To.Add("csunny95@yahoo.com.hk");
+            mail.Subject = "Test";
+            mail.Body = "Hellow World";
+            emailServer.Send(mail);
+
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
