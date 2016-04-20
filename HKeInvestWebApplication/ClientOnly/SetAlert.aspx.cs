696eec7243a5bfe2acb2cb5cf1b9055b7fc8d82e
TrudyLeung <trudy.leung@gmail.com>
2016-04-20 20:49:19 +0800

    update

--- HKeInvestWebApplication/ClientOnly/SetAlert.aspx.cs
+++ HKeInvestWebApplication/ClientOnly/SetAlert.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
+using System.Data.SqlClient;
 using HKeInvestWebApplication.Code_File;
 using HKeInvestWebApplication.ExternalSystems.Code_File;
 
@@ -37,17 +38,47 @@ namespace HKeInvestWebApplication.ClientOnly
                 return;
             }
             else
+            {
                 args.IsValid = true;
+                return;
+            }
         }
 
         protected void cvSet_ServerValidate(object source, ServerValidateEventArgs args)
         {
-
+            string alerttype = AlertType_RadioButtonList.SelectedItem.Text.Trim();
+            string sql = "SELECT alertType FROM Alert WHERE accountNumber = (SELECT accountNumber FROM Account WHERE userName ='" + User.Identity.Name + "')";
+            DataTable dtAlert = myHKeInvestData.getData(sql);
+            args.IsValid = false;
+            foreach (DataRow row in dtAlert.Rows)
+            {
+                if (row["alertType"].ToString() == alerttype)
+                {
+                    args.IsValid = true;
+                    return;
+                }
+            }
         }
 
         protected void Set_Click(object sender, EventArgs e)
         {
-            
+            string alerttype = AlertType_RadioButtonList.SelectedItem.Text.Trim();
+            string securityType = ddlSecurityType.SelectedItem.Text.Trim();
+            string input = SecurityCode.Text.Trim();
+            string value = AlertValue.Text.Trim();
+
+            string sql = "SELECT alertType FROM Alert WHERE accountNumber = (SELECT accountNumber FROM Account WHERE userName ='" + User.Identity.Name + "')";
+            DataTable dtAlert = myHKeInvestData.getData(sql);
+           
+            foreach (DataRow row in dtAlert.Rows)
+            {
+                if (row["alertType"].ToString() == alerttype)
+                    return;
+            }
+
+            string sql2 = "INSERT INTO [Alert]('(SELECT accountNumber FROM Account WHERE userName ='" + User.Identity.Name + "')', 'alerttype', 'securityType', 'input', 'value')";
+            SqlTransaction trans = myHKeInvestData.beginTransaction();
+            myHKeInvestData.setData(sql2, trans);   
         }
     }
 }
