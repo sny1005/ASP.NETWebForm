eaee69054f5f3f982bb3175aa71dc0e17383a200
Long Yin CHOW <lychowaa@connect.ust.hk>
2016-04-24 03:19:13 +0800

    Highlight cell from database test

--- HKeInvestWebApplication/Calendar.aspx.cs
+++ HKeInvestWebApplication/Calendar.aspx.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
+using System.Data.SqlClient;
+using HKeInvestWebApplication.Code_File;
+
 
 namespace HKeInvestWebApplication
 {
@@ -12,10 +15,27 @@ namespace HKeInvestWebApplication
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            HKeInvestData myHKeInvestData = new HKeInvestData();
+            DataTable eventinfo = myHKeInvestData.getData("Select year, month, day From Events");
             SelectedDatesCollection eventdates = Calendar1.SelectedDates;
-            eventdates.Add(new DateTime(2016, 4, 24));
-            eventdates.Add(new DateTime(2016, 4, 25));
-            eventdates.Add(new DateTime(2016, 4, 26));
+            //for( int i=1; i<= eventid.Rows.Count; i++ )
+            foreach(DataRow row in eventinfo.Rows)
+            {
+                /*DataTable year = myHKeInvestData.getData("Select year from Events where Eventid = '" + i + "'");
+                DataTable month= myHKeInvestData.getData("Select month from Events where Eventid = '" + i + "'");
+                DataTable day = myHKeInvestData.getData("Select day from Events where Eventid = '" + i + "'");*/
+                string year_string = row[0].ToString();
+                int year = Int32.Parse(year_string);
+                string month_string = row[1].ToString();
+                int month = Int32.Parse(month_string);
+                string day_string = row[2].ToString();
+                int day = Int32.Parse(year_string);
+
+                eventdates.Add(new DateTime(year,month,day));
+
+            }
+            
+
 
         }
 
