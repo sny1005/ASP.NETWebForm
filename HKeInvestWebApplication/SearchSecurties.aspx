24ac0ead6f905a0ebeca4172dbb0dfdc29b7e89f
dm1319 <dm1319@live.hk>
2016-04-26 01:17:36 +0800

    Initializations of 3.Securities searching (Not yet deal with code in cs file)

---
+++ HKeInvestWebApplication/SearchSecurties.aspx
@@ -0,0 +1,94 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SearchSecurties.aspx.cs" Inherits="HKeInvestWebApplication.SearchSecurties" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h10>Search Securites</h10>
+    <div class="form-horizontal">
+
+        <div class="form-group">
+            <asp:Label runat="server" Text="Security Type : " CssClass="control-label col-md-2" ></asp:Label>
+            <asp:DropDownList ID="ddlSecurityType" runat="server" AutoPostBack="True">
+                <asp:ListItem Value="0">Security type</asp:ListItem>
+                <asp:ListItem Value="bond">Bond</asp:ListItem>
+                <asp:ListItem Value="stock">Stock</asp:ListItem>
+                <asp:ListItem Value="unit trust">Unit Trust</asp:ListItem>
+            </asp:DropDownList>
+        </div>
+
+        <div class="form-group">
+            <asp:Label runat="server" Text="Security Name : " CssClass="control-label col-md-2" AssociatedControlID="SecName"></asp:Label>
+            <asp:TextBox ID="SecName" runat="server" CssClass="form-control"></asp:TextBox>
+            <br />
+            <asp:Label runat="server" Text="Secutity Code : " CssClass="control-label col-md-2" AssociatedControlID="SecCode"></asp:Label>
+            <asp:TextBox ID="SecCode" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+
+<%--    <div class="form-group">
+            <asp:DropDownList ID="ddlCurrency" runat="server" AutoPostBack="True" Visible="False" >
+                <asp:ListItem Value="0">Currency</asp:ListItem>
+            </asp:DropDownList>
+        </div>--%>
+
+<%--        <div class="form-group">
+            <asp:Label ID="lblClientName" runat="server" Text="" Visible="False" CssClass="col-md-3"></asp:Label>
+            <asp:Label ID="lblResultMessage" runat="server" Text="" Visible="False"></asp:Label>
+        </div>--%>
+       
+        <div class="form-group">
+         <div>
+            <asp:GridView ID="StockGV" runat="server" Visible="False" AutoGenerateColumns="False" OnSorting="StockGV_Sorting" CellPadding="5">
+                <Columns>
+                    <asp:BoundField DataField="StockCode" HeaderText="Stock code" ReadOnly="True" SortExpression="code" />
+                    <asp:BoundField DataField="StockName" HeaderText="Stock Name" ReadOnly="True" SortExpression="name" />
+                    <asp:BoundField DataField="RecentClosing" DataFormatString="{0:n2}" HeaderText="Most recent closing price per share" ReadOnly="True" SortExpression="recentclosing" />
+                    <asp:BoundField DataField="LastChange" DataFormatString="{0:n2}" HeaderText="Last trading day change" ReadOnly="True" SortExpression="lastchange" />
+                    <asp:BoundField DataField="LastPercent" DataFormatString="{0:n2}" HeaderText="Last trading day percentage change" ReadOnly="True" SortExpression="lastpercentage" />
+                    <asp:BoundField DataField="LastVolume" DataFormatString="{0:n2}" HeaderText="Last trading day volume of shares" ReadOnly="True" SortExpression="lastvolume" />
+                    <asp:BoundField DataField="HighPrice" DataFormatString="{0:n2}" HeaderText="highprice" ReadOnly="True" />
+                    <asp:BoundField DataField="LowPrice" DataFormatString="{0:n2}" HeaderText="lowprice" ReadOnly="True" />
+                    <asp:BoundField DataField="PE" DataFormatString="{0:n2}" HeaderText="Price earnings ratio of the stock" ReadOnly="True" SortExpression="pe" />
+                    <asp:BoundField DataField="Yield" DataFormatString="{0:n2}" HeaderText="Yield of the stock" ReadOnly="True" SortExpression="yield" />
+                </Columns>
+            </asp:GridView>
+        </div>
+        <div>
+            <asp:GridView ID="BondGV" runat="server" Visible="False" AutoGenerateColumns="False" OnSorting="BondGV_Sorting" CellPadding="5">
+                <Columns>
+                    <asp:BoundField DataField="BondCode" HeaderText="Bond Code" ReadOnly="True" SortExpression="code" />
+                    <asp:BoundField DataField="Bondname" HeaderText="Bond Name" ReadOnly="True" SortExpression="name" />
+                    <asp:BoundField DataField="Launch" HeaderText="Launch date" ReadOnly="True" SortExpression="date" />
+                    <asp:BoundField DataField="Base" HeaderText="Base" ReadOnly="True" SortExpression="base"/>
+                    <asp:BoundField DataField="Value" DataFormatString="{0:n2}" HeaderText="Total Monetary value" ReadOnly="True" SortExpression="value" />
+                    <asp:BoundField DataField="Price" DataFormatString="{0:n2}" HeaderText="Current price per share" ReadOnly="True" SortExpression="price"/>
+                    <asp:BoundField DataField="CAGP06" DataFormatString="{0:n2}" HeaderText="Compound annual growth percentage last six months" ReadOnly="True" SortExpression="cagp06"/>
+                    <asp:BoundField DataField="CAGP12" DataFormatString="{0:n2}" HeaderText="Compound annual growth percentage last one year" ReadOnly="True" SortExpression="cagp12"/>
+                    <asp:BoundField DataField="CAGP36" DataFormatString="{0:n2}" HeaderText="Compound annual growth percentage last three years" ReadOnly="True" SortExpression="cagp36"/>
+                    <asp:BoundField DataField="CAGP00" DataFormatString="{0:n2}" HeaderText="Compound annual growth percentage since the bond was launched" ReadOnly="True" SortExpression="cagp00"/>
+                </Columns>
+            </asp:GridView>
+        </div>
+
+         <div>
+            <asp:GridView ID="UnitTrustGV" runat="server" Visible="False" AutoGenerateColumns="False" OnSorting="UnitTrustGV_Sorting" CellPadding="5">
+                <Columns>
+                    <asp:BoundField DataField="UnitTrustCode" HeaderText="Code" ReadOnly="True" SortExpression="code" />
+                    <asp:BoundField DataField="UnitTrustName" HeaderText="Name" ReadOnly="True" SortExpression="name" />
+                    <asp:BoundField DataField="Launch" HeaderText="Launch date" ReadOnly="True" SortExpression="date" />
+                    <asp:BoundField DataField="Base" HeaderText="Base" ReadOnly="True" SortExpression="base"/>
+                    <asp:BoundField DataField="Value" DataFormatString="{0:n2}" HeaderText="Total Monetary value" ReadOnly="True" SortExpression="value" />
+                    <asp:BoundField DataField="Price" DataFormatString="{0:n2}" HeaderText="Current price per share" ReadOnly="True" SortExpression="price"/>
+                    <asp:BoundField DataField="Rating" HeaderText="Risk/return rating" ReadOnly="True" SortExpression="rating"/>
+                    <asp:BoundField DataField="CAGP06" DataFormatString="{0:n2}" HeaderText="Compound annual growth percentage last six months" ReadOnly="True" SortExpression="cagp06"/>
+                    <asp:BoundField DataField="CAGP12" DataFormatString="{0:n2}" HeaderText="Compound annual growth percentage last one year" ReadOnly="True" SortExpression="cagp12"/>
+                    <asp:BoundField DataField="CAGP36" DataFormatString="{0:n2}" HeaderText="Compound annual growth percentage last three years" ReadOnly="True" SortExpression="cagp36"/>
+                    <asp:BoundField DataField="CAGP00" DataFormatString="{0:n2}" HeaderText="Compound annual growth percentage since the bond was launched" ReadOnly="True" SortExpression="cagp00"/>
+                </Columns>
+            </asp:GridView>
+        </div>
+
+
+
+
+
+    </div>
+    </div>
+</asp:Content>
---
+++ HKeInvestWebApplication/SearchSecurties.aspx.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+using HKeInvestWebApplication.Code_File;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace HKeInvestWebApplication
+{
+    public partial class ApplicationPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void cvAcNo_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            string acn = AccountNumber.Text.Trim();
+            string lastname = LastName.Text.Trim();
+            lastname = lastname.ToUpper();
+
+            if (lastname.Length > 1)
+            {
+                lastname = lastname.Substring(0, 2);
+                if (!acn.Contains(lastname))
+                {
+                    args.IsValid = false;
+                    cvAcNo.ErrorMessage = "Account Number must start with the first two letters of your Last Name in uppercase.";
+                    return;
+                }
+            }
+            else
+            {
+                if (acn[0] != acn[1] || acn[0] != lastname[0])
+                {
+                    args.IsValid = false;
+                    cvAcNo.ErrorMessage = "As your last name contains only 1 character, account number must start with repeating the character twice.";
+                    return;
+                }
+            }
+
+            Regex exp = new Regex(@"^\w{2}\d{8}$");
+            if (exp.IsMatch(acn)) return;
+            else
+            {
+                args.IsValid = false;
+                cvAcNo.ErrorMessage = "Account Number must be followed by 8 digits";
+                return;
+            }
+        }
+
+        protected void cvDOB_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            string dob = DateOfBirth.Text.Trim();
+
+            Regex exp = new Regex(@"^\d{2}\/\d{2}\/\d{4}$");
+            if (!exp.IsMatch(dob))
+            {
+                args.IsValid = false;
+                cvDOB.ErrorMessage = "Date of Birth is not valid.";
+                cvDOB2.ErrorMessage = "Co-account holder date of Birth is not valid.";
+                return;
+            }
+
+            int day = Convert.ToInt32(dob.Substring(0, 2));
+            int month = Convert.ToInt32(dob.Substring(3, 2));
+            int year = Convert.ToInt32(dob.Substring(6, 4));
+            
+            if (day > 0 && day <= 31)
+            {
+                if (month > 0 && month <= 12)
+                {
+                    if (year > 1900 && year <= 2016) return;
+                }
+            }
+            args.IsValid = false;
+            cvDOB.ErrorMessage = "Date of Birth is not valid.";
+            cvDOB2.ErrorMessage = "Co-account holder date of Birth is not valid.";
+            return;
+        }
+
+        protected void cvPhone_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if (hPhone.Text != "" || hFax.Text != "" || bPhone.Text != "" || bFax.Text != "")
+                return;
+            args.IsValid = false;
+            cvPhone.ErrorMessage = "At least one of the phone numbers must be filled in.";
+            cvPhone2.ErrorMessage = "At least one of the co-account holder's phone numbers must be filled in.";
+        }
+
+        protected void cvIssueCountry_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if (ID_PP.SelectedValue == "Passport Number" && IssueCountry.Text == "")
+            {
+                args.IsValid = false;
+                cvIssueCountry.ErrorMessage = "Passport country of issue is required.";
+                return;
+            }
+        }
+        protected void cvIssueCountry2_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if (ID_PP2.SelectedValue == "Passport Number" && IssueCountry2.Text == "")
+            {
+                args.IsValid = false;
+                cvIssueCountry2.ErrorMessage = "Co-account holder passport country of issue is required.";
+                return;
+            }
+        }
+
+        protected void cvOccupation_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if (EmpStatus.SelectedValue == "Employed")
+            {
+                if (args.Value == "") args.IsValid = false;
+            }
+        }
+        protected void cvOccupation2_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if (EmpStatus2.SelectedValue == "Employed")
+            {
+                if (args.Value == "") args.IsValid = false;
+            }
+        }
+
+        protected void cvSpecificSource_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if (PrimarySource.SelectedValue == "other" && args.Value == "") args.IsValid = false;
+        }
+
+        protected void cvDeposit_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            //none selected
+            if(!Cheque.Checked && !Transfer.Checked)
+            {
+                args.IsValid = false;
+                cvDeposit.ErrorMessage = "Please choose one or more payment method.";
+                return;
+            }
+
+            //cheque selected but no value
+            if(Cheque.Checked && ChequeV.Text == "")
+            {
+                args.IsValid = false;
+                cvDeposit.ErrorMessage = "Please indicate the amount you would like to pay by cheque";
+                return;
+            }
+
+            //transfer selected but no value
+            if (Transfer.Checked && TransferV.Text == "")
+            {
+                args.IsValid = false;
+                cvDeposit.ErrorMessage = "Please indicate the amount you would like to transfer";
+                return;
+            }
+
+            //make sure amount >=20000
+            decimal temp = 0;
+            if (Cheque.Checked) temp = Convert.ToDecimal(ChequeV.Text);
+            if(Transfer.Checked) temp = temp + Convert.ToDecimal(TransferV.Text);
+            if (temp >= 20000) return;
+            
+            args.IsValid = false;
+            cvDeposit.ErrorMessage = "A HK$20,000 minimum deposit is required to open an account.";
+            return;
+        }
+
+        protected void Register_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                HKeInvestData myHKeInvest = new HKeInvestData();
+                string acNo = AccountNumber.Text;
+
+                decimal initialBalance;
+                if (Transfer.Checked && Cheque.Checked)         //both contain value
+                    initialBalance = Convert.ToDecimal(TransferV.Text) + Convert.ToDecimal(ChequeV.Text);
+                else if (Transfer.Checked)                      //tranfer contains value
+                    initialBalance = Convert.ToDecimal(TransferV.Text);
+                else                                            //only chequeV contain value
+                    initialBalance = Convert.ToDecimal(ChequeV.Text);
+
+                //the account will be successfully updated
+                string sql = "INSERT INTO [Account] VALUES ('" + acNo + "', '" + acType.SelectedValue + "', " + initialBalance + ", NULL)";
+
+                SqlTransaction myTransaction = myHKeInvest.beginTransaction();
+                myHKeInvest.setData(sql, myTransaction);
+
+                //insert all required fields first
+                sql = "INSERT INTO [Client] (title, accountNumber, firstName, lastName, dateOfBirth, email, HKIDPassportNumber, citizenship, residence, building, street, district, employmentStatus, employByBroker, publiclyTradedCompany, primarySourceFund, investObjective, investKnowledge, investExperience, annualIncome, liquidNetWorth, freeBalanceToFund) ";
+                sql = sql + "VALUES ('" + title.SelectedValue + "', '" + acNo + "', '" + FirstName.Text + "', '" + LastName.Text + "', CONVERT(date, '" + DateOfBirth.Text + "', 103), '" + Email.Text + "', '" + HKID.Text + "', '" + Citizen.Text + "', '" + Residence.Text + "', '" + Building.Text + "', '" + Street.Text + "', '" + District.Text + "', '" + EmpStatus.SelectedValue + "', '" + EmpByBroker.SelectedValue + "', '" + CompanyDirector.SelectedValue + "', '" + PrimarySource.SelectedValue + "', '" + Objective.SelectedValue + "', '" + Knowledge.SelectedValue + "', '" + Experience.SelectedValue + "', '" + Income.SelectedValue + "', '" + NetWorth.SelectedValue + "', '" + Fund.Checked + "')";
+                myHKeInvest.setData(sql, myTransaction);
+
+                //insert optional fields
+                //insert passport number if needed
+                if (ID_PP.SelectedValue == "Passport Number")
+                {
+                    sql = "UPDATE [Client] SET passportIssueCountry = '" + IssueCountry.Text + "' WHERE accountNumber = '" + acNo + "'";
+                    myHKeInvest.setData(sql, myTransaction);
+                }
+
+                //insert phone numbers
+                if (hPhone.Text != "")
+                {
+                    sql = "UPDATE [Client] SET homePhone = '" + hPhone.Text + "' WHERE accountNumber = '" + acNo + "'";
+                    myHKeInvest.setData(sql, myTransaction);
+                }
+                if (hFax.Text != "")
+                {
+                    sql = "UPDATE [Client] SET homeFax = '" + hFax.Text + "' WHERE accountNumber = '" + acNo + "'";
+                    myHKeInvest.setData(sql, myTransaction);
+                }
+                if (bPhone.Text != "")
+                {
+                    sql = "UPDATE [Client] SET businessPhone = '" + bPhone.Text + "' WHERE accountNumber = '" + acNo + "'";
+                    myHKeInvest.setData(sql, myTransaction);
+                }
+                if (bFax.Text != "")
+                {
+                    sql = "UPDATE [Client] SET businessFax = '" + bFax.Text + "' WHERE accountNumber = '" + acNo + "'";
+                    myHKeInvest.setData(sql, myTransaction);
+                }
+
+                //insert employmeny information
+                if(EmpStatus.SelectedValue == "Employed")
+                {
+                    sql = "UPDATE [Client] SET occupation = '" + Occupation.Text + "', yearsWithEmployer = '" + yrWithEmp.Text + "', employerName = '" + Employer.Text + "', employerPhone = '" + EmployerPhone.Text + "', businessNature = '" + Business.Text + "' WHERE accountNumber = '" + acNo + "'";
+                    myHKeInvest.setData(sql, myTransaction);
+                }
+
+                //insert specific primary source fof fund
+                if (PrimarySource.SelectedValue == "other")
+                {
+                    sql = "UPDATE [Client] SET specificSource = '" + SpecificSource.Text + "' WHERE accountNumber = '" + acNo + "'";
+                    myHKeInvest.setData(sql, myTransaction);
+                }
+
+                //end of primary account holder infomation
+                myHKeInvest.commitTransaction(myTransaction);
+
+
+                //
+                //INSERT CO HOLDER'S INFORMATION
+                //
+                if (acType.SelectedIndex != 0)
+                {
+                    myTransaction = myHKeInvest.beginTransaction();
+                    
+                    //insert all required fields first
+                    sql = "INSERT INTO [Client] (title, accountNumber, firstName, lastName, dateOfBirth, email, HKIDPassportNumber, citizenship, residence, building, street, district, employmentStatus, employByBroker, publiclyTradedCompany, primarySourceFund, investObjective, investKnowledge, investExperience, annualIncome, liquidNetWorth, freeBalanceToFund) ";
+                    sql = sql + "VALUES ('" + title2.SelectedValue + "', '" + acNo + "', '" + FirstName2.Text + "', '" + LastName2.Text + "', CONVERT(date, '" + DateOfBirth2.Text + "', 103), '" + Email2.Text + "', '" + HKID2.Text + "', '" + Citizen2.Text + "', '" + Residence2.Text + "', '" + Building2.Text + "', '" + Street2.Text + "', '" + District2.Text + "', '" + EmpStatus2.SelectedValue + "', '" + EmpByBroker2.SelectedValue + "', '" + CompanyDirector2.SelectedValue + "', '" + PrimarySource.SelectedValue + "', '" + Objective.SelectedValue + "', '" + Knowledge.SelectedValue + "', '" + Experience.SelectedValue + "', '" + Income.SelectedValue + "', '" + NetWorth.SelectedValue + "', '" + Fund.Checked + "')";
+                    myHKeInvest.setData(sql, myTransaction);
+                    myHKeInvest.commitTransaction(myTransaction);       //need to commit transaction before being able to retreive information from the database
+
+
+                    //get co-ac holder's clientNumber
+                    sql = "SELECT clientNumber from Client WHERE accountNumber = '" + acNo + "' AND firstName = '" + FirstName2.Text + "'";
+                    DataTable dtClient = myHKeInvest.getData(sql);
+                    string cNo = "";
+                    foreach(DataRow row in dtClient.Rows)
+                    {
+                        cNo = Convert.ToString(row["clientNumber"]);
+                    }
+
+                    myTransaction = myHKeInvest.beginTransaction();
+                    //insert optional fields
+                    //insert passport number if needed
+                    if (ID_PP2.SelectedValue == "Passport Number")
+                    {
+                        sql = "UPDATE [Client] SET passportIssueCountry = '" + IssueCountry2.Text + "' WHERE clientNumber = '" + cNo + "'";
+                        myHKeInvest.setData(sql, myTransaction);
+                    }
+
+                    //insert phone numbers
+                    if (hPhone2.Text != "")
+                    {
+                        sql = "UPDATE [Client] SET homePhone = '" + hPhone2.Text + "' WHERE clientNumber = '" + cNo + "'";
+                        myHKeInvest.setData(sql, myTransaction);
+                    }
+                    if (hFax2.Text != "")
+                    {
+                        sql = "UPDATE [Client] SET homeFax = '" + hFax2.Text + "' WHERE clientNumber = '" + cNo + "'";
+                        myHKeInvest.setData(sql, myTransaction);
+                    }
+                    if (bPhone2.Text != "")
+                    {
+                        sql = "UPDATE [Client] SET businessPhone = '" + bPhone2.Text + "' WHERE clientNumber = '" + cNo + "'";
+                        myHKeInvest.setData(sql, myTransaction);
+                    }
+                    if (bFax2.Text != "")
+                    {
+                        sql = "UPDATE [Client] SET businessFax = '" + bFax2.Text + "' WHERE clientNumber = '" + cNo + "'";
+                        myHKeInvest.setData(sql, myTransaction);
+                    }
+
+                    //insert employmeny information
+                    if (EmpStatus2.SelectedValue == "Employed")
+                    {
+                        sql = "UPDATE [Client] SET occupation = '" + Occupation2.Text + "', yearsWithEmployer = '" + yrWithEmp2.Text + "', employerName = '" + Employer2.Text + "', employerPhone = '" + EmployerPhone2.Text + "', businessNature = '" + Business2.Text + "' WHERE clientNumber = '" + cNo + "'";
+                        myHKeInvest.setData(sql, myTransaction);
+                    }
+
+                    //insert specific primary source fof fund
+                    if (PrimarySource.SelectedValue == "other")
+                    {
+                        sql = "UPDATE [Client] SET specificSource = '" + SpecificSource.Text + "' WHERE clientNumber = '" + cNo + "'";
+                        myHKeInvest.setData(sql, myTransaction);
+                    }
+
+                    //END of optional fields for co-ac holder
+                    myHKeInvest.commitTransaction(myTransaction);
+                }
+            }
+        }
+
+        protected void acType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (acType.SelectedIndex == 0)
+                CoHolderPanel.Visible = false;
+            else
+                CoHolderPanel.Visible = true;
+        }
+    }
+}
